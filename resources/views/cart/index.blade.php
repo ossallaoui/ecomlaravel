f789a33868b57e0e6ea1eaa73bcf86d4ef45ad81
unknown <ossallaoui>
2020-07-05 12:08:18 +0000

    price format

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 
+<div class="col-md-12">
+    <h1>Mon panier</h1>
+</div>
 @if (Cart::count() > 0)
    <div class="pb-5">
     <div class="container">
@@ -43,7 +46,7 @@
                     	<form action="{{ route('cart.destroy', $product->rowId) }}" method="POST">
                     		@csrf
                     		@method('DELETE')
-                    		<button type="submit" class="text-dark"><i class="fa fa-trash"></i></button>
+                    		<button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i></button>
                     	</form>
                     </td>
                 </tr>
@@ -54,7 +57,40 @@
           <!-- End -->
         </div>
       </div>
-
+			<div class="row py-5 p-4 bg-white rounded shadow-sm">
+	            <div class="col-lg-6">
+	            <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Coupon code</div>
+	            <div class="p-4">
+	                <p class="font-italic mb-4">If you have a coupon code, please enter it in the box below</p>
+	                <div class="input-group mb-4 border rounded-pill p-2">
+	                <input type="text" placeholder="Apply coupon" aria-describedby="button-addon3" class="form-control border-0">
+	                <div class="input-group-append border-0">
+	                    <button id="button-addon3" type="button" class="btn btn-dark px-4 rounded-pill"><i class="fa fa-gift mr-2"></i>Apply coupon</button>
+	                </div>
+	                </div>
+	            </div>
+	            <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Instructions for seller</div>
+	            <div class="p-4">
+	                <p class="font-italic mb-4">If you have some information for the seller you can leave them in the box below</p>
+	                <textarea name="" cols="30" rows="2" class="form-control"></textarea>
+	            </div>
+	            </div>
+	            <div class="col-lg-6">
+	            <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Détails de la commande </div>
+	            <div class="p-4">
+	                <p class="font-italic mb-4">Shipping and additional costs are calculated based on values you have entered.</p>
+	                <ul class="list-unstyled mb-4">
+	                <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Sous-total </strong><strong>{{ getPrice(Cart::subtotal()) }}</strong></li>
+	                {{-- <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Shipping and handling</strong><strong>$10.00</strong></li> --}}
+	                <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Taxe</strong><strong>{{ getPrice(Cart::tax()) }}</strong></li>
+	                <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Total</strong>
+	                    <h5 class="font-weight-bold">{{ getPrice(Cart::Total()) }}</h5>
+	                </li>
+	                </ul><a href="#" class="btn btn-dark rounded-pill py-2 btn-block">Passer à la caisse</a>
+	            </div>
+	          </div>
+            </div>
+        </div>
 
     </div>
   </div>
